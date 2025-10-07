7f19d9e4b8e1b35d63fde9123267cee5ab63f18c
nhanbernie <ttnhanbernie@gmail.com>
2025-10-07 14:47:50 +0700

    fix: param for admin page

--- src/app/(authenticated)/admin/user/[userId]/page.tsx
+++ src/app/(authenticated)/admin/user/[userId]/page.tsx
@@ -4,13 +4,14 @@ import React from "react";
 import AdminTutorDetailPage from "@/features/admin/tutor-detail/AdminTutorDetailPage";
 
 interface UserDetailPageProps {
-  params: {
+  params: Promise<{
     userId: string;
-  };
+  }>;
 }
 
-const UserDetailPage: React.FC<UserDetailPageProps> = ({ params }) => {
-  return <AdminTutorDetailPage tutorId={params.userId} />;
+const UserDetailPage: React.FC<UserDetailPageProps> = async ({ params }) => {
+  const { userId } = await params;
+  return <AdminTutorDetailPage tutorId={userId} />;
 };
 
 export default UserDetailPage;
